6c6e9e418305af12ba82a12ff1952bfe8f902005
Max <max.zvyagintsev.1988@gmail.com>
2019-09-12 16:19:33 +0300

    tesntt

--- math.func.cpp
+++ math.func.cpp
@@ -15,6 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Topcalc.  If not, see <http://www.gnu.org/licenses/>.
 
+
 #include "math.func.hpp"
 #include "error.hpp"
 #include "math.util.hpp"
